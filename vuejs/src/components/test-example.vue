c319c61cac7fa979875c96707b1da19ed1a0562f
HINAKO <artstudy.sun175@gmail.com>
2022-09-07 11:46:45 +0900

    vueのtest(mocha)のためのファイル追加

---
+++ vuejs/src/components/test-example.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="counter">
+    <h1>FizzBuzz</h1>
+    <h2>{{ count }}</h2>
+    <button id="button" v-on:click="count+=1">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter-test',
+  data() {
+    return {
+      count : 0
+    }
+  }
+}
+</script>
