1f4fbe86fa996e0ff00f19101e744bfe5265926d
suuuuetsugu <suuuuetsugu@gmail.com>
2022-09-08 09:35:36 +0900

    add signup

---
+++ next/pages/signup.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useState } from "react";
+
+export default function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const data = {
+      "name": name,
+      "email": email,
+      "password": password
+    };
+    axios.post(`http://127.0.0.1:8000/api/signup`, data)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
+  return (
+    <>
+    <form>
+        <label>name:</label>
+        <input type="name" onChange={(e) => setName(e.target.value)}/>
+        <label>email:</label>
+        <input type="email" onChange={(e) => setEmail(e.target.value)}/>
+        <label>password:</label>
+        <input type="password"  onChange={(e) => setPassword(e.target.value)}/>
+        <button onClick={handleSubmit}>新規登録</button>
+    </form>
+    </>
+  )
+}
